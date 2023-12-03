bdf1510b7ec1f2ee12a1c2ccfb5f7e8035b0b74e
Peter Huynh <peterhyh@yahoo.com>
2023-12-02 19:38:37 -0600

    speed change

--- ball.py
+++ ball.py
@@ -23,7 +23,7 @@ class Ball(Turtle):
 
     def bounce_x(self):
         self.x_move *= -1
-        self.ball_speed *= 0.9
+        self.ball_speed *= 0.5
 
     def increase_speed(self):
         self.ball_speed += 1
